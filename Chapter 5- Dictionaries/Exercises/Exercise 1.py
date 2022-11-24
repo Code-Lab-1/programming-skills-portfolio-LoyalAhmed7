52f59ed5b0e83c39cdcf0c7bfdeea04c671f0997
Ahmed Raheem <114246808+LoyalAhmed7@users.noreply.github.com>
2022-11-24 22:33:19 +0400

    Create Exercise 1.py

---
+++ Chapter 5- Dictionaries/Exercises/Exercise 1.py
@@ -0,0 +1,8 @@
+person = {'first_name':'Ahmed','last_name': 'Raheem','age': 19 ,'city': 'Sharjah',
+}
+
+
+print(person['first_name'])
+print(person['last_name'])
+print(person['age'])
+print(person['city'])
