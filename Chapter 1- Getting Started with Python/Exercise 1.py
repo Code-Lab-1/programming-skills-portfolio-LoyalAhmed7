e19ab2c33e228867b19716830b7cdd7ceddb5ea9
Ahmed Raheem <114246808+LoyalAhmed7@users.noreply.github.com>
2022-11-25 02:24:20 +0400

    Delete Exercise 1.py

--- Chapter 1- Getting Started with Python/Exercise 1.py
+++
@@ -1,7 +0,0 @@
-print("Exercise 1\n")
-print("""Twinkle, twinkle, little star,
-How I wonder what you are! 
-Up above the world so high, 
-Like a diamond in the sky. 
-Twinkle, twinkle, little star,
-How I wonder what you are """)
