a26685d491f75290fa3d74f2edb4cfd73fda2016
Ahmed Raheem <114246808+LoyalAhmed7@users.noreply.github.com>
2022-11-24 21:07:08 +0400

    Create Exercise 1.py

---
+++ Chapter 4- Control Flow/Exercises/Exercise 1.py
@@ -0,0 +1,7 @@
+alien_color = 'blue'
+if alien_color == 'blue':
+  print('You have just earned 5 points for killing the alien')
+
+alien_color= 'blue'
+if alien_color == 'green':
+  print('You have just earned 5 points for killing the alien')
