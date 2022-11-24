a42d2fdfe9f3538306273aeae2d5cc9b76fa1841
Ahmed Raheem <114246808+LoyalAhmed7@users.noreply.github.com>
2022-11-24 19:04:58 +0400

    Create Exercise 2.py

---
+++ Chapter 3- Data Structures/Exercises/Exercise 2.py
@@ -0,0 +1,6 @@
+names=['Ahmed','Raheem','Ali','Muhammed','Khaled']
+print('hi ' + names[0] + " how're you?")
+print('hi ' + names[1] + " how're you?")
+print('hi ' + names[2] + " how're you?")
+print('hi ' + names[3] + " how're you?")
+print('hi ' + names[4] + " how're you?")
