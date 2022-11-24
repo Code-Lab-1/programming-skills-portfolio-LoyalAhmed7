8881088c40348c6a00ea7e30e645b741b6eb796d
Ahmed Raheem <114246808+LoyalAhmed7@users.noreply.github.com>
2022-11-24 21:16:57 +0400

    Update Exercise 2.py

--- Chapter 4- Control Flow/Exercises/Exercise 2.py
+++ Chapter 4- Control Flow/Exercises/Exercise 2.py
@@ -3,3 +3,9 @@ if alien_color == 'green':
   print('you have just earned 5 points for killing the alien')
 else:
   print('you have earned 10 points')
+
+alien_color = 'blue'                                                 
+if alien_color == 'green':
+  print('you have just earned 5 points for killing the alien')
+else:
+  print('you have earned 10 points')
